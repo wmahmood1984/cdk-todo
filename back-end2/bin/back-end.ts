b86f6327f4a2bda81833bae7973134d3fca7b120
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-12-23 22:37:09 +0500

    add short id

--- back-end2/bin/back-end.ts
+++ back-end2/bin/back-end.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
+import 'source-map-support/register';
 import { BackEndStack } from '../lib/back-end-stack';
 
 const app = new cdk.App();
--- back-end2/lib/back-end-stack.ts
+++ back-end2/lib/back-end-stack.ts
@@ -4,10 +4,20 @@ import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as events from "@aws-cdk/aws-events";
 import * as targets from "@aws-cdk/aws-events-targets";
+import * as s3 from '@aws-cdk/aws-s3'
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import * as CodePipeline from '@aws-cdk/aws-codepipeline'
+import * as CodePipelineAction from '@aws-cdk/aws-codepipeline-actions'
+import * as CodeBuild from '@aws-cdk/aws-codebuild'
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 
 export class BackEndStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
 
    // Creates the AppSync API
    const api = new appsync.GraphqlApi(this, 'Api', {
@@ -127,6 +137,118 @@ notesTable.grantFullAccess(echoLambda)
 // Create an environment variable that we will use in the function code
 notesLambda.addEnvironment('NOTES_TABLE', notesTable.tableName);
 echoLambda.addEnvironment('NOTES_TABLE', notesTable.tableName);
+
+
+//Deploy Gatsby on s3 bucket
+const myBucket = new s3.Bucket(this, "GATSBYbuckets", {
+  versioned: true,       
+  websiteIndexDocument: "index.html"
+});
+
+
+const dist = new cloudfront.Distribution(this, 'myDistribution', {
+  defaultBehavior: { origin: new origins.S3Origin(myBucket) }
+});
+
+new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
+  sources: [s3Deployment.Source.asset("../front-end2/public")],
+  destinationBucket: myBucket,
+  distribution: dist
+});
+
+new cdk.CfnOutput(this, "CloudFrontURL", {
+  value: dist.domainName
+});
+
+const sourceOutput = new CodePipeline.Artifact();
+
+// Artifact from build stage
+const S3Output = new CodePipeline.Artifact();
+
+//Code build action, Here you will define a complete build
+const s3Build = new CodeBuild.PipelineProject(this, 's3Build', {
+  buildSpec: CodeBuild.BuildSpec.fromObject({
+    version: '0.2',
+    phases: {
+      install: {
+        "runtime-versions": {
+          "nodejs": 12
+        },
+        commands: [
+          
+          'cd front-end2',
+          'npm i -g gatsby',
+          'npm install',
+        ],
+      },
+      build: {
+        commands: [
+          'gatsby build',
+        ],
+      },
+    },
+    artifacts: {
+      'base-directory': './front-end2/public',   ///outputting our generated Gatsby Build files to the public directory
+      "files": [
+        '**/*'
+      ]
+    },
+  }),
+  environment: {
+    buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0,   ///BuildImage version 3 because we are using nodejs environment 12
+  },
+});
+
+const policy :any = new PolicyStatement();
+    policy.addActions('s3:*');
+    policy.addResources('*');
+
+    s3Build.addToRolePolicy(policy);
+
+    const pipeline = new CodePipeline.Pipeline(this, 'GatsbyPipeline', {
+      crossAccountKeys: false,  //Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1.
+      restartExecutionOnUpdate: true,  //Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
+    });
+const oauth : any = cdk.SecretValue.secretsManager('GitHubToken2')
+
+    //First Stage Source
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new CodePipelineAction.GitHubSourceAction({
+          actionName: 'Checkout',
+          owner: 'wmahmood1984',
+          repo: "cdk-todo",
+          oauthToken: oauth, ///create token on github and save it on aws secret manager
+          output: sourceOutput,                                       ///Output will save in the sourceOutput Artifact
+          branch: "master",                                           ///Branch of your repo
+        }),
+      ],
+    })
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new CodePipelineAction.CodeBuildAction({
+          actionName: 's3Build',
+          project: s3Build,
+          input: sourceOutput,
+          outputs: [S3Output],
+        }),
+      ],
+    })
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [
+        new CodePipelineAction.S3DeployAction({
+          actionName: 's3Build',
+          input: S3Output,
+          bucket: myBucket,
+        }),
+      ],
+    })
+
 
 }
 }
